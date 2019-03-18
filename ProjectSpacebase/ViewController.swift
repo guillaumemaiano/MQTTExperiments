10628909a5643d826f79c2ef08edbcaa82030972
guillaume MAIANO <drunkenmaster@kungfu.cn>
2019-03-18 13:04:59 +0100

    Experiments with MQTT

--- ProjectSpacebase/ViewController.swift
+++ ProjectSpacebase/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import CocoaMQTT
 
+// very unfancy "learning project" following https://medium.com/thefloatingpoint/mqtt-in-ios-d8574b55e006
 class ViewController: UIViewController {
+
+    @IBOutlet weak var mqttLabel: UILabel!
+
+    var mqtt: CocoaMQTT?
+    // improvement: pull the server URL from a secret
+    let mqttHostStr = "localhost"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupMQTT()
     }
 
-
+    // maybe have a setupMQTT method that returns the setup client, and another that uses it to connect?
+    func setupMQTT() {
+        let clientID = "CocoaMQTT-" + String(ProcessInfo().processIdentifier)
+        // port 1883 is standard as per https://mqtt.org/faq
+        let standardPort: UInt16 = 1883
+        mqtt = CocoaMQTT(clientID: clientID, host: mqttHostStr, port: standardPort)
+        // authentication added in v3.1
+        mqtt?.username = "test"
+        mqtt?.password = "public"
+        // last will cannot be changed later: https://stackoverflow.com/questions/49163609/change-will-on-a-mqtt-connection
+        // further explained: https://www.hivemq.com/blog/mqtt-essentials-part-9-last-will-and-testament/
+        mqtt?.willMessage = CocoaMQTTWill( topic: "/will", message: "dieout")
+        mqtt?.keepAlive = 60
+        mqtt?.connect()
+    }
 }
 
