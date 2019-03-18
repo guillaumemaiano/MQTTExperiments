bf7991c95fe603b780e027981924c0bb83534009
guillaume MAIANO <drunkenmaster@kungfu.cn>
2019-03-18 13:50:15 +0100

    Ends the tutorial on subscription with CocoaMQTT

---
+++ ProjectSpacebase/ViewController+MQTTDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController+MQTTDelegate.swift
+//  ProjectSpacebase
+//
+//  Created by guillaume MAIANO on 18/03/2019.
+//  Copyright © 2019 guillaume MAIANO. All rights reserved.
+//
+import UIKit
+import CocoaMQTT
+
+extension ViewController: CocoaMQTTDelegate {
+
+    // These two methods are all we care about for now
+    func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
+        // we could keep track of the connection state of the MQTT object
+        // but it's simpler to just do the subscribe here in this learning project
+        // we subscribe to a topic signalling user presence
+        // possible implementations: ESP8266 + IR sensors, or simply publishing a "I'm there" message to the topic.
+        self.mqtt?.subscribe("presence")
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        // since the message can carry a payload, we could pass relevant data as well
+        if let msgString = message.string {
+            mqttLabel.text = msgString
+        }
+    }
+
+    // Other required methods for CocoaMQTTDelegate
+    func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
+        completionHandler(true)
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+    }
+
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+    }
+
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+    }
+
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+    }
+
+    func _console(_ info: String) {
+    }
+
+}
--- ProjectSpacebase/ViewController.swift
+++ ProjectSpacebase/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mqttLabel: UILabel!
 
+    // improvement: instead of optional, build it as a lazy var, and connect at the end of the didLoad
     var mqtt: CocoaMQTT?
     // improvement: pull the server URL from a secret
     let mqttHostStr = "localhost"
@@ -32,6 +33,8 @@ class ViewController: UIViewController {
         // authentication added in v3.1
         mqtt?.username = "test"
         mqtt?.password = "public"
+        // for this learning project, the view controller is its own delegate (as per an extension file)
+        mqtt?.delegate = self
         // last will cannot be changed later: https://stackoverflow.com/questions/49163609/change-will-on-a-mqtt-connection
         // further explained: https://www.hivemq.com/blog/mqtt-essentials-part-9-last-will-and-testament/
         mqtt?.willMessage = CocoaMQTTWill( topic: "/will", message: "dieout")
@@ -39,4 +42,3 @@ class ViewController: UIViewController {
         mqtt?.connect()
     }
 }
-
